efdbacef81640503419a477cf60a69457b31b239
LaStr <LaStr>
2020-06-26 17:02:09 +0200

    put model data in index.html

--- src/main/java/cz/lastr/WebVsRssDiff/Controller/IndexController.java
+++ src/main/java/cz/lastr/WebVsRssDiff/Controller/IndexController.java
@@ -1,12 +1,16 @@
 package cz.lastr.WebVsRssDiff.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class IndexController {
 
+    @GetMapping
     public String Index(Model model){
 
-        model.addAttribute("articles", "article");
+        model.addAttribute("articles", "contents of articles");
 
         return "index";
     }
