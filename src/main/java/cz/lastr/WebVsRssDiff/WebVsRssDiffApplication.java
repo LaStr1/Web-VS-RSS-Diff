e3d5e84edf8c7427c668bed7fd136e304915df49
LaStr <LaStr>
2020-06-26 16:32:17 +0200

    init commit

---
+++ src/main/java/cz/lastr/WebVsRssDiff/WebVsRssDiffApplication.java
@@ -0,0 +1,13 @@
+package cz.lastr.WebVsRssDiff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebVsRssDiffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebVsRssDiffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/lastr/WebVsRssDiff/WebVsRssDiffApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.lastr.WebVsRssDiff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebVsRssDiffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
