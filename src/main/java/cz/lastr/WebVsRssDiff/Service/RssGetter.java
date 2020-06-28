2513368aa8fa8b6c86db7bcf5d0206390056cf00
LaStr <LaStr>
2020-06-28 16:48:54 +0200

    refactor

--- src/main/java/cz/lastr/WebVsRssDiff/Service/RssGetter.java
+++ src/main/java/cz/lastr/WebVsRssDiff/Service/RssGetter.java
@@ -6,6 +6,7 @@ import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -13,20 +14,36 @@ import java.util.List;
 
 public class RssGetter {
 
-    URL url;
-
-    public RssGetter(URL url){
-        this.url = url;
+    public RssGetter(){
     }
 
-    public List<Integer> getRss() throws IOException, FeedException {
+    public List<Integer> getArticlesFromRSS() throws IOException, FeedException {
+        URL url = new URL("https://ihned.cz/?m=rss");
+        List<Integer> articlesFromRSS;
+
         SyndFeedInput input = new SyndFeedInput();
         SyndFeed feed = input.build(new XmlReader(url));
 
-        List<SyndEntry> items = feed.getEntries();
+        articlesFromRSS = parseRssFeed(feed);
+
+        return articlesFromRSS;
+    }
+
+    public List<Integer> parseRssFeed(SyndFeed feed) throws IOException, FeedException {
+        List<SyndEntry> itemsInFeed;
+        List<Integer> articlesFromRSS;
+
+        itemsInFeed = feed.getEntries();
+
+        articlesFromRSS = getArticlesFromFeed(itemsInFeed);
+
+        return articlesFromRSS;
+    }
+
+    private List<Integer> getArticlesFromFeed(List<SyndEntry> itemsInFeed) {
         List<Integer> articlesFromRSS = new ArrayList<>();
 
-        for (SyndEntry item : items){
+        for (SyndEntry item : itemsInFeed){
             int indexOfDash = item.getUri().indexOf("-") + 1;
 
             String articleItemInString = item.getUri().substring(indexOfDash);
--- src/test/java/cz/lastr/WebVsRssDiff/Service/RssGetterTest.java
+++ src/test/java/cz/lastr/WebVsRssDiff/Service/RssGetterTest.java
@@ -1,8 +1,12 @@
 package cz.lastr.WebVsRssDiff.Service;
 
+import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.FeedException;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -15,10 +19,14 @@ class RssGetterTest {
 
     @Test
     public void testRssGetterIsNotNull() throws IOException, FeedException {
-        URL url = new URL("https://ihned.cz/?m=rss");
+        String fileWithRssFeedInXML = "src/test/java/cz/lastr/WebVsRssDiff/Service/rssFeedExample.xml";
+        FileInputStream fileInputStream = new FileInputStream(fileWithRssFeedInXML);
 
-        rssGetter = new RssGetter(url);
-        assertNotNull(rssGetter.getRss());
+        SyndFeedInput input = new SyndFeedInput();
+        SyndFeed feed = input.build(new XmlReader(fileInputStream));
+
+        rssGetter = new RssGetter();
+        assertNotNull(rssGetter.parseRssFeed(feed));
     }
 
 }
