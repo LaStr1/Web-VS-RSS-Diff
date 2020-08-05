018df001cfcb156745d8de2838d2a0cac9e37728
LaStr <LaStr>
2020-08-05 10:42:24 +0200

    add Save Article To Regular Table Test, change MySql dialect in application.properties for debugging

--- src/main/java/cz/lastr/WebVsRssDiff/Service/RssArticleService.java
+++ src/main/java/cz/lastr/WebVsRssDiff/Service/RssArticleService.java
@@ -1,25 +1,39 @@
 package cz.lastr.WebVsRssDiff.Service;
 
+import cz.lastr.WebVsRssDiff.Model.RssArticle;
 import cz.lastr.WebVsRssDiff.ModelForTempTable.RssArticleTempTable;
+import cz.lastr.WebVsRssDiff.Repository.RssArticleRepository;
 import cz.lastr.WebVsRssDiff.RepositoryForTempTables.RssArticleRepositoryTempTable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class RssArticleService {
 
-    private RssArticleRepositoryTempTable articleRepositoryTempTable;
+    private RssArticleRepositoryTempTable rssArticleRepositoryTempTable;
+    private RssArticleRepository rssArticleRepository;
 
-    public RssArticleService(RssArticleRepositoryTempTable articleRepositoryTempTable){
-        this.articleRepositoryTempTable = articleRepositoryTempTable;
+    public RssArticleService(RssArticleRepositoryTempTable rssArticleRepositoryTempTable, RssArticleRepository rssArticleRepository) {
+        this.rssArticleRepositoryTempTable = rssArticleRepositoryTempTable;
+        this.rssArticleRepository = rssArticleRepository;
     }
 
     public List<RssArticleTempTable> findAll(){
-        return articleRepositoryTempTable.findAll();
+        return rssArticleRepositoryTempTable.findAll();
+    }
+
+    public List<RssArticle> findAllInRegularTable(){
+        return rssArticleRepository.findAll();
     }
 
     public void saveToTempTable(List<RssArticleTempTable> articles){
-        articleRepositoryTempTable.saveAll(articles);
+        rssArticleRepositoryTempTable.saveAll(articles);
+    }
+
+    public void saveToRegularTable(List<RssArticle> articles){
+        rssArticleRepository.saveAll(articles);
     }
 }
---
+++ src/test/java/cz/lastr/WebVsRssDiff/Repository/RssServiceTests/SaveArticleToRegularTableTest.java
@@ -0,0 +1,38 @@
+package cz.lastr.WebVsRssDiff.Repository.RssServiceTests;
+
+import cz.lastr.WebVsRssDiff.Model.RssArticle;
+import cz.lastr.WebVsRssDiff.Model.WebArticle;
+import cz.lastr.WebVsRssDiff.Service.RssArticleService;
+import cz.lastr.WebVsRssDiff.Service.WebArticleService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@Transactional
+public class SaveArticleToRegularTableTest {
+
+    @Autowired
+    RssArticleService rssArticleService;
+
+    @Test
+    public void saveArticleToRegularTable() {
+        RssArticle article1 = new RssArticle(
+                66783210);
+
+        List<RssArticle> articles = new ArrayList<>();
+        articles.add(article1);
+
+        rssArticleService.saveToRegularTable(articles);
+
+        List<RssArticle> articlesFromTable = rssArticleService.findAllInRegularTable();
+
+        assertTrue(articlesFromTable.contains(article1));
+    }
+}
